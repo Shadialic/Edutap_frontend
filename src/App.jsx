18486caf6d88bc3eb3fbef6032aa1bb4dd43aa9f
shadil <shadilsa786@gmai.com>
2024-03-07 22:42:59 +0530

    appjsx correct it

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import UserRouter from "./routes/UserRouter";
 import TutorRouter from "./routes/TutorRouter";
 import AdminRoutes from "./routes/AdminRoutes";
 
-
-
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/*" element={<UserRouter />} />
+        <Route path="/" element={<UserRouter />} />
+        
         <Route path="/vendor/*" element={<TutorRouter />} />
+        
         <Route path="/admin/*" element={<AdminRoutes />} />
       </Routes>
     </Router>
