6848b956c1e22ef67c588554aaf9c2c0ae73bfca
shadil <shadilsa786@gmai.com>
2024-03-07 18:05:13 +0530

    admin route

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import UserRouter from "./routes/UserRouter";
 import TutorRouter from "./routes/TutorRouter";
 import AdminRouter from "./routes/AdminRouter";
 
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/*" element={<UserRouter />} />
+        
         <Route path="/vendor/*" element={<TutorRouter />} />
         <Route path="/admin/*" element={<AdminRouter />} />
       </Routes>
